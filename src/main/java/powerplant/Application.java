a2ac037b49d8864d19a35905e024122cd968aff2
Abhishek <abhtri2009@gmail.com>
2020-07-03 17:47:20 +0530

    Influxdb

--- src/main/java/powerplant/Application.java
+++ src/main/java/powerplant/Application.java
@@ -1,11 +1,17 @@
 package powerplant;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import powerplant.service.TestService;
 
 @SpringBootApplication
+//@EnableScheduling
 public class Application {
+
     public static void main(String args[]){
         SpringApplication.run(Application.class);
+
     }
 }
